8ef4bc305925698d67b8740dc0578e0752d84912
Wolmer Martins <wolmermartins@hotmail.com>
2024-08-23 18:20:16 -0300

    feat: creates custom fizzbuzz problem solution console app

---
+++ TwistedFizzBuzzCustomProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿using TwistedFizzBuzzLib.Implementations;
+using TwistedFizzBuzzLib.Models;
+
+var printer = new Printer();
+var twistedFizzBuzz = new TwistedFizzBuzz(printer, -20, 127);
+
+twistedFizzBuzz.WithTwists(
+    new List<Twist>
+    {
+        new Twist("Fizz", 5),
+        new Twist("Buzz", 9),
+        new Twist("Bar", 27)
+    });
+
+twistedFizzBuzz.Execute();
